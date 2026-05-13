1205d4dffea41ec545d540345363211b080ce90d
Kriac <1651151664@qq.com>
2026-05-13 11:49:08 +0800

    style(commitlint): reorganize PluginOptions interface properties order

--- packages/commitlint/src/types.ts
+++ packages/commitlint/src/types.ts
@@ -8,8 +8,8 @@ export interface Optional {
 
 /** 插件选项 */
 export interface PluginOptions {
-  /** 扩展作用域 */
-  scopes?: Optional[];
   /** 自定义规则 */
   rules?: UserConfig["rules"];
+  /** 扩展作用域 */
+  scopes?: Optional[];
 }
