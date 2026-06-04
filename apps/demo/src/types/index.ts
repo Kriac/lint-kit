4bdd37de72f471a71c5ecb4200abc6eedc483c49
Kriac <1651151664@qq.com>
2026-06-04 16:26:49 +0800

    feat(demo): add scss support and update type test cases

--- apps/demo/src/types/index.ts
+++ apps/demo/src/types/index.ts
@@ -1,20 +1,17 @@
-import type { AllowedAttrs } from "vue";
+import type { Ref } from "vue";
 import { computed, ref } from "vue";
 
 /** 测试类型A */
 export type TestTypeA = "a0" | "a1" | "a2";
 
 /** 测试类型B */
-export type TestTypeB = "b0" | "b1" | "b2";
-
-/** 测试类型C */
-export type TestTypeC = "c0" | "c1" | "c2";
+export type TestTypeB<T> = "b0" | "b1" | "b2" | T;
 
 /** 测试联合类型排序 */
-export type TestUnionType = TestTypeA | TestTypeB | TestTypeC;
+export type TestUnionType = Ref<string, number> | TestTypeA | TestTypeB<string>;
 
 /** 测试交叉类型排序 */
-export type TestIntersectionType = TestTypeA & TestTypeB & TestTypeC;
+export type TestIntersectionType = TestTypeA & TestTypeB<string>;
 
 /** 测试对象类型排序 */
 export type TestSortObjectTypes = {
@@ -31,4 +28,4 @@ export interface TestSortInterfaces {
 }
 
 // 测试命名导出排序
-export { computed, ref, AllowedAttrs };
+export { computed, ref };
