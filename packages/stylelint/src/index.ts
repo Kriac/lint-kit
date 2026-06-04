bf9b0bd765423dac8515d9e467fa50cadf7efad3
Kriac <1651151664@qq.com>
2026-06-04 16:26:35 +0800

    feat(stylelint): upgrade to 17.12.0 and refine config

--- packages/stylelint/src/index.ts
+++ packages/stylelint/src/index.ts
@@ -4,28 +4,27 @@ const require = createRequire(import.meta.url);
 
 export default {
   extends: [
-    "stylelint-config-standard",
-    "stylelint-config-standard-scss",
     "stylelint-config-recommended-vue",
+    "stylelint-config-standard-scss",
     "stylelint-config-recess-order",
   ],
+  plugins: ["stylelint-order"],
   overrides: [
     {
       files: ["**/*.html", "**/*.vue"],
       customSyntax: require.resolve("postcss-html"),
     },
   ],
-  plugins: ["stylelint-order"],
   rules: {
-    // 空文件校验 - 禁用原因: 部分组件可能没有样式
+    // 关闭空文件校验，部分组件可能没有样式
     "no-empty-source": null,
-    // 未知选择器校验 - 禁用原因: 不同平台标签可能不同
+    // 关闭未知选择器校验，不同平台标签可能不同
     "selector-type-no-unknown": null,
-    // 类名格式校验 - 禁用原因: 类名有很多设计规范，不做约束
+    // 关闭类名格式校验，类名有很多设计规范，不做约束
     "selector-class-pattern": null,
-    // 重复选择器校验 - 禁用原因: 部分情况下可以更好的维护代码
+    // 关闭重复选择器校验，部分情况下可以更好的维护代码
     "no-duplicate-selectors": null,
-    // 字体兜底校验 - 禁用原因: 字体图标不能兜底
+    // 关闭字体兜底校验，字体图标不能兜底
     "font-family-no-missing-generic-family-keyword": null,
   },
 };
